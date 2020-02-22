0758ecf6e78bce7ed2b68d407f51651eddba7b34
brendan hales <brendan_hales@hotmail.com>
2020-02-22 23:58:19 +0800

    homepage layout

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+import Directory from '../components/directory/directory-component';
+
+const homePage = () =>{
+    return(
+        <div className='homepage'>
+            <h1>Welcome to my Homepage</h1>
+            <Directory></Directory>
+        </div>
+    )
+}
+
+export default homePage;
