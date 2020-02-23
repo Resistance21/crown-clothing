976a5f966777e45e645d728d7ac6551d6213f851
brendan hales <brendan_hales@hotmail.com>
2020-02-24 01:10:33 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import {Route} from 'react-router-dom';
 import HomePage from './pages/home-page/homepage.component';
 import ShopPage from './pages/shop-page/shopPage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-    <switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </switch>
+      <Header />
+      <switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </switch>
     </div>
   );
 }
