aefba7613eaba28edcc4f24f02ca65467c24fda1
brendan hales <brendan_hales@hotmail.com>
2020-02-24 03:36:10 +0800

    added sign in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route} from 'react-router-dom';
 import HomePage from './pages/home-page/homepage.component';
 import ShopPage from './pages/shop-page/shopPage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+
+const SignInAndSignUp = () => {
+    return(
+        <SignIn />
+
+    )
+} 
+
+export default SignInAndSignUp;
